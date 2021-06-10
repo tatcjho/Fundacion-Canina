f2bc6007bf75b955e5387e49835d78d7de22328a
Tatiana Cardenas <tatycjho@gmail.com>
2021-06-09 23:47:34 -0500

    redireccion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Home', url: '/folder/Inbox', icon: 'home' },
+    { title: 'Quiénes Somos', url: '/folder/Outbox', icon: 'happy' },
+    { title: '¿Cómo ayudar?', url: '/folder/Favorites', icon: 'information' },
+    { title: 'Alianzas', url: '/folder/Archived', icon: 'heart' },
+    { title: 'Contáctanos', url: '/folder/Trash', icon: 'flash' },
+    { title: 'Salir', url: '/folder/Spam', icon: 'log-out' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = ['Inicio', 'Quienes-Somos', 'Como-Ayudar', 'Alianzas', 'Contáctanos', 'Salir'];
   constructor() {}
 }
